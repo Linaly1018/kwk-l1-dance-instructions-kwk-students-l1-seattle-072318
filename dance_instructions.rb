08cd8365c6b32b8abff928688dedc5fb38a0a18c
Linaly1018 <miyamoto.linaly@gmail.com>
2019-01-17 17:18:37 +0000

    Automatically backed up by Learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,44 @@
 
-# Code your methods below
+def starting_stance
+  puts "Plant your legs apart"
+end
+  
+def base_footwork
+    puts "The Base Footwork"
+end
+  
+  
+def skip_step 
+    puts "Steps for Skip Step Left and Right"
+    puts "Step 1: Lower left foot to the ground."
+    puts "Step 2: Bounce left foot back up slightly, kicking it a few inches back"
+    puts "Step 3: Left, Right, Left and left."
+    puts "Step 4: Lower right foot to the ground."
+    puts "Step 5: Bounce right foot back up slightly, kicking it a few inches back."
+    puts "Steo 6: Right, Left, Right and Right"
+end
+  
+def bOB_THE_REINS
+    puts "Step 1: Holding your arms out in front of you, straight and at chest level."
+    puts "Step 2: Cross your right wrist over your left and hold them together."
+    puts "Step 3: Left your arms up and down in a loose bouncing movenment, in time with the beat of the song. This movement is repeated eight times."
+end
+  
+def lasso 
+    puts "Step 1: Start arm movement by holding left arm so the back of your wrist is near your chin, pointinhg your left elbow straight left."
+    puts "Step 2: Lift right arm up and point right elbow diagonally to the right."
+    puts "Step 3: Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def bust_a_move
+  puts "Now create your own move!"
+  puts "Be cool"
+  puts "Be snazzy!"
+end
+
+puts starting_stance
+puts bust_a_move
+puts base_footwork
+
+puts bOB_THE_REINS
+puts skip_step
